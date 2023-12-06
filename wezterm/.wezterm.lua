525d16035a230af54ce629008c484a95263de053
Sean McKenzie <70048908+seanmckenzie428@users.noreply.github.com>
2023-12-06 03:25:17 -0700

    Add kitty and wezterm configs

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,57 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+
+
+
+
+-- This is where you actually apply your config choices
+
+
+-- ***** THEME *****
+-- wezterm.gui is not available to the mux server, so take care to
+-- do something reasonable when this config is evaluated by the mux
+function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
+
+function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'OneDark (base16)'
+  else
+    return 'OneLight (base16)'
+  end
+end
+
+config.color_scheme = scheme_for_appearance(get_appearance())
+-- ***** END THEME *****
+
+
+config.adjust_window_size_when_changing_font_size = false
+config.cursor_blink_rate = 0
+config.font_size = 13
+config.line_height = 1.2
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
+config.window_padding = {
+  left = "2cell",
+  right = "2cell",
+  top = "0.5cell",
+  bottom = "0.5cell",
+}
+config.use_resize_increments = true
+
+-- and finally, return the configuration to wezterm
+return config
