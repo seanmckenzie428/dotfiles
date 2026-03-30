76f94bf2e1e49d682d3e92d1e5f8e960b80b4aa8
Sean McKenzie <70048908+seanmckenzie428@users.noreply.github.com>
2026-03-30 15:01:58 -0600

    Update brew-bundle.sh

--- run_onchange_after_brew-bundle.sh
+++ run_onchange_after_brew-bundle.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# .Brewfile hash: {{ include ".Brewfile" | sha256sum }}
+# Including the hash forces this script to run whenever .Brewfile is updated
 brew bundle --file="~/.Brewfile"
